18011bd0fb74baeca4406a1bc7aa91464070089a
Matthias Mak <ulm.university.news@gmail.com>
2015-12-28 20:27:24 +0100

    Announcement list adapter and list item created.

--- app/src/main/java/ulm/university/news/app/controller/AnnouncementFragment.java
+++ app/src/main/java/ulm/university/news/app/controller/AnnouncementFragment.java
@@ -1,12 +1,18 @@
 package ulm.university.news.app.controller;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
@@ -15,12 +21,22 @@ import ulm.university.news.app.R;
 import ulm.university.news.app.api.ChannelAPI;
 import ulm.university.news.app.data.Announcement;
 import ulm.university.news.app.manager.database.ChannelDatabaseManager;
+import ulm.university.news.app.manager.database.DatabaseLoader;
 
-public class AnnouncementFragment extends Fragment {
+public class AnnouncementFragment extends Fragment implements LoaderManager.LoaderCallbacks<List<Announcement>> {
     /** This classes tag for logging. */
     private static final String TAG = "AnnouncementFragment";
 
-    private ChannelDatabaseManager channelDBM;
+    private AnnouncementListAdapter listAdapter;
+    private DatabaseLoader<List<Announcement>> databaseLoader;
+    private List<Announcement> announcements;
+    private int channelId;
+
+    private ListView lvAnnouncements;
+    private TextView tvInfo;
+
+    /** The loader's id. This id is specific to the ChannelFragment's LoaderManager. */
+    private static final int LOADER_ID = 3;
 
     public AnnouncementFragment() {
         // Required empty public constructor
@@ -37,17 +53,30 @@ public class AnnouncementFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        channelDBM = new ChannelDatabaseManager(getActivity());
-        int channelId = getArguments().getInt("channelId");
-        int messageNumber = channelDBM.getMaxMessageNumberAnnouncement(channelId);
+        // Initialize or reuse an existing database loader.
+        getActivity().getSupportLoaderManager().initLoader(LOADER_ID, null, this);
+
+        announcements = new ArrayList<>();
+        listAdapter = new AnnouncementListAdapter(getActivity(), R.layout.announcement_list_item, announcements);
+
+        channelId = getArguments().getInt("channelId");
+        int messageNumber = databaseLoader.getChannelDBM().getMaxMessageNumberAnnouncement(channelId);
         ChannelAPI.getInstance(getActivity()).getAnnouncements(channelId, messageNumber);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_announcement, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_announcement, container, false);
+        lvAnnouncements = (ListView) view.findViewById(R.id.fragment_announcement_lv_announcements);
+        tvInfo = (TextView) view.findViewById(R.id.fragment_announcement_tv_info);
+
+        lvAnnouncements.setAdapter(listAdapter);
+
+        if (announcements != null && !announcements.isEmpty()) {
+            tvInfo.setVisibility(View.GONE);
+            lvAnnouncements.setVisibility(View.VISIBLE);
+        }
+        return view;
     }
 
     @Override
@@ -78,7 +107,7 @@ public class AnnouncementFragment extends Fragment {
 
         // Store new announcements.
         for (Announcement announcement : announcements) {
-            channelDBM.storeAnnouncement(announcement);
+            databaseLoader.getChannelDBM().storeAnnouncement(announcement);
             authorIds.add(announcement.getAuthorModerator());
         }
 
@@ -87,5 +116,52 @@ public class AnnouncementFragment extends Fragment {
             Log.d(TAG, "authorId:" + authorId);
             // TODO Check moderator existence, load and store if necessary.
         }
+    }
+
+    @Override
+    public Loader<List<Announcement>> onCreateLoader(int id, Bundle args) {
+        databaseLoader = new DatabaseLoader<>(getActivity(), new DatabaseLoader
+                .DatabaseLoaderCallbacks<List<Announcement>>() {
+            @Override
+            public List<Announcement> onLoadInBackground() {
+                // Load all announcements of a specific channels.
+                return databaseLoader.getChannelDBM().getAnnouncements(channelId);
+            }
+
+            @Override
+            public IntentFilter observerFilter() {
+                // Listen to database changes on channel subscriptions.
+                IntentFilter filter = new IntentFilter();
+                filter.addAction(ChannelDatabaseManager.STORE_ANNOUNCEMENT);
+                return filter;
+            }
+        });
+        // This loader uses the channel database manager to load data.
+        databaseLoader.setChannelDBM(new ChannelDatabaseManager(getActivity()));
+        return databaseLoader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Announcement>> loader, List<Announcement> data) {
+        // Update list.
+        announcements = data;
+        listAdapter.setData(data);
+        listAdapter.notifyDataSetChanged();
+        // Update view.
+        if (announcements.isEmpty()) {
+            lvAnnouncements.setVisibility(View.GONE);
+            tvInfo.setText(getText(R.string.fragment_announcement_list_empty));
+            tvInfo.setVisibility(View.VISIBLE);
+        } else {
+            lvAnnouncements.setVisibility(View.VISIBLE);
+            tvInfo.setVisibility(View.GONE);
+            tvInfo.setText(getText(R.string.fragment_announcement_list_loading));
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Announcement>> loader) {
+        // Clear adapter data.
+        listAdapter.setData(null);
     }
 }
---
+++ app/src/main/java/ulm/university/news/app/controller/AnnouncementListAdapter.java
@@ -0,0 +1,76 @@
+package ulm.university.news.app.controller;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.util.List;
+
+import ulm.university.news.app.R;
+import ulm.university.news.app.data.Announcement;
+
+/**
+ * TODO
+ *
+ * @author Matthias Mak
+ */
+public class AnnouncementListAdapter extends ArrayAdapter<Announcement> {
+    public AnnouncementListAdapter(Context context, int resource) {
+        super(context, resource);
+    }
+
+    public AnnouncementListAdapter(Context context, int resource, List<Announcement> announcements) {
+        super(context, resource, announcements);
+    }
+
+    /**
+     * Updates the data of the AnnouncementListAdapter.
+     *
+     * @param data The updated announcement list.
+     */
+    public void setData(List<Announcement> data) {
+        clear();
+        if (data != null) {
+            for (int i = 0; i < data.size(); i++) {
+                add(data.get(i));
+            }
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return super.getCount();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (convertView == null) {
+            LayoutInflater vi;
+            vi = LayoutInflater.from(getContext());
+            convertView = vi.inflate(R.layout.announcement_list_item, parent, false);
+        }
+
+        Announcement announcement = getItem(position);
+
+        if (announcement != null) {
+            TextView tvFirstLine = (TextView) convertView.findViewById(R.id.announcement_list_view_tv_title);
+            TextView tvSecondLine = (TextView) convertView.findViewById(R.id.announcement_list_view_tv_text);
+            TextView tvThirdLine = (TextView) convertView.findViewById(R.id.announcement_list_view_tv_date);
+
+            // Format the date for output.
+            DateTimeFormatter dtfOut = DateTimeFormat.forPattern("MM/dd/yyyy HH:mm");
+
+            tvFirstLine.setText(announcement.getTitle());
+            tvSecondLine.setText(announcement.getText());
+            tvThirdLine.setText(dtfOut.print(announcement.getCreationDate()));
+        }
+        return convertView;
+    }
+}
